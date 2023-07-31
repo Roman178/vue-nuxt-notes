f43d644e61e503e1477c25de5080c8766cc9a72f
Roman <ismagilov771@gmail.com>
2023-07-31 13:55:39 +0400

    fix

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,5 +1,9 @@
 export default {
   // Global page headers: https://go.nuxtjs.dev/config-head
+  target: 'static',
+  router: {
+    base: '/vue-nuxt-notes/'
+  },
   head: {
     title: "vue-notes",
     htmlAttrs: {
